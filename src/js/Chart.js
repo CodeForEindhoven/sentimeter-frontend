36a2a84b62fd899ac0016c7b6d7aef5e69dcc543
Marcel Goethals <marcel.goethals@gmail.com>
2017-01-25 23:47:27 +0100

    chartxy

--- src/js/Chart.js
+++ src/js/Chart.js
@@ -17,11 +17,21 @@ var ChartCmp = (function(){
 	return {
 		controller: function(history){
 
+			return {
+
+			};
 		},
 		view: function(ctrl, history){
+			var start =  new Date(history[0].timestamp).getTime();
+			var length = new Date(history[history.length-1].timestamp).getTime()-start;
+
 			if(history){
 				return m("div", {class: s.parent}, history.map(function(s){
-					return m("span", s.score + " - ");
+					var time = new Date(s.timestamp).getTime();
+					var x = ((time-start)/length)*80+10;
+					var y = s.score*8+1;
+
+					return m("span", {style: "position: absolute; left:"+x+"%; bottom:"+y+"%"},s.score);
 				}));
 			}
 			return m("div");
